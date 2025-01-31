2db1b8a5ff51fccacb87ec255bdf6df80323dbfe
Oliver Gudenschwager <oliver.gudenschwager@etu.univ-amu.fr>
2025-01-31 16:08:33 +0100

    Légèrs changements d'espace et syntaxe en Circle et helloWorld

--- src/exercice2/Circle.java
+++ src/exercice2/Circle.java
@@ -7,18 +7,18 @@ public class Circle {
 	public Circle() {
 	      radius = 1.0;
 	      color = "red";
-	   }
+	}
 	   
-	   public Circle(double r) {
-	      radius = r;
-	      color = "red";
-	   }
+	public Circle(double r) {
+	   radius = r;
+	   color = "red";
+	}
 	   
-	   public double getRadius() {
-	     return radius; 
-	   }
+	public double getRadius() {
+	  return radius; 
+	}
 	   
-	   public double getArea() {
-	      return radius*radius*Math.PI;
-	   }
+	public double getArea() {
+	   return radius*radius*Math.PI;
+	}
 }
--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,9 +1,5 @@
-
 public class helloWorld {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.print("Hello World!");
 	}
-
 }
